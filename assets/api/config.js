c989fcbf2b3ecb59b56e45bc40f898a882c421ee
maya-dn <trsignals@gmail.com>
2026-03-31 12:38:50 -0700

    changing pythonURI

--- assets/api/config.js
+++ assets/api/config.js
@@ -2,5 +2,5 @@
   if (location.hostname === "localhost" || location.hostname === "127.0.0.1") {
       pythonURI = "http://localhost:8327";  // Same URI for localhost or 127.0.0.1
   } else {
-      pythonURI = "https://pwc.opencodingsociety.com";
+      pythonURI = "https://pwc-flask.opencodingsociety.com";
   }
